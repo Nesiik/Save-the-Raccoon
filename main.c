5336f6b300d94bb209a67d5827b7d72463f118c8
Leo JACQUOT <leo.jacquot1@etu.univ-lorraine.fr>
2023-09-14 17:36:11 +0200

    debut structure du jeu + echap & quit

--- main.c
+++ main.c
@@ -1,5 +1,23 @@
 #include <SDL.h>
 #include <SDL_ttf.h>
+#include "world.h"
+
+void init(world_t * world){
+    init_data(world);
+}
+
+void handle_events(SDL_Event *event,world_t *world){
+    while( SDL_PollEvent( event ) ) {
+        if( event->type == SDL_QUIT ) {
+            world->gameover = ForceQuit;
+        }
+        if(event->type == SDL_KEYDOWN){
+            if(event->key.keysym.sym == SDLK_ESCAPE){
+                world->gameover = ForceQuit;
+            }
+        }
+    }
+}
 
 int main( int argc, char* args[] )
 {
@@ -20,6 +38,13 @@ int main( int argc, char* args[] )
     SDL_RenderPresent(renderer);
 
     SDL_Delay(3000);
+
+    SDL_Event event;
+    world_t world;
+
+    while(!is_game_over(&world)){
+        handle_events(&event,&world);   
+    }
 
     SDL_DestroyWindow(window);
     SDL_Quit();
---
+++ world.h
@@ -0,0 +1,27 @@
+enum GameState {
+    Menu = -1, 
+    Alive = 0, 
+    Dead = 1,   
+    Win = 2,    
+    ForceQuit = 3
+};
+
+struct sprite_s{
+    int x; /*!< Abs */
+    int y; /*!< Ord */
+    int h; /*!< Height */
+    int w; /*!< Width */
+};
+typedef struct sprite_s sprite_t;
+
+
+struct world_s{
+    sprite_t player;
+    int level;
+    char gameover; /*!< -1 = Menu; 0 = alive; 1 = Dead; 2 = Win; 3 = ForceQuit */
+};
+typedef struct world_s world_t;
+
+void init_data(world_t * world);
+
+int is_game_over(world_t *world);
