8bbffcc450302aba19074d06255f5db2b4d6bbf3
Justin MARTIN <justin.martin9@etu.univ-lorraine.fr>
2023-09-14 16:27:49 +0200

    test

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
-#include "SDL.h"
-
+#include <SDL.h>
+#include <SDL_ttf.h>
 
 int main( int argc, char* args[] )
 {
---
+++ sdl2.sh
@@ -0,0 +1,2 @@
+sudo apt-get install libsdl2-dev
+sudo apt-get install libsdl-ttf2.0-dev
