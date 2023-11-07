d62b8213864c9dc7efe705f54681e1b0d58dbb50
Justin MARTIN <justin.martin9@etu.univ-lorraine.fr>
2023-11-07 16:11:11 +0100

    noettoyage main

--- main.c
+++ main.c
@@ -18,41 +18,13 @@ int main( int argc, char* args[] )
     TTF_Init();
 
     SDL_Window* window = create_window();
-
     SDL_Renderer *renderer = create_renderer(window);
 
-    //Uint64 lastUp,end,delta;
-    //double MSPerFrame,fps;
-
     init(renderer,ressources,world);
-
-    //lastUp = SDL_GetTicks64();
-    //set_vsync(renderer,SDL_FALSE);
 
     while(!fin(world)){
         SDL_RenderClear(renderer);
         handle_events(event,world,ressources);
-        /*
-        for (size_t i = 0; i < 1000; i++)
-        {           
-            SDL_Rect fillRect = { SCREEN_WIDTH / 4, SCREEN_HEIGHT / 4, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 };
-            SDL_SetRenderDrawColor( renderer, 0xFF, 0xFF, 0xFF, SDL_ALPHA_OPAQUE );        
-            SDL_RenderFillRect( renderer, &fillRect );
-            SDL_SetRenderDrawColor( renderer, 0x00, 0x00, 0x00, SDL_ALPHA_OPAQUE );  
-        }
-        */
-        /*
-        end = SDL_GetTicks64();
-
-        delta = end - lastUp;
-        printf("%lu , %lu \n", lastUp,end); 
-        int taille = snprintf(NULL,0, "%lu", delta);
-        char* str = malloc(taille + 1);
-        snprintf(str,taille + 1, "%lu", delta);
-        afficher_texte(renderer,font,str,0,0);
-        SDL_RenderPresent(renderer);
-        lastUp = SDL_GetTicks64();
-        */
         switch (world->gameover)
         {
             case Menu:
