06b227f8e6091616c729ef18a4e94deffd204824
Justin MARTIN <justin.martin9@etu.univ-lorraine.fr>
2023-12-14 22:52:34 +0100

    comment "unusable" fonction

--- option.h
+++ option.h
@@ -27,7 +27,7 @@ typedef struct option_s
 }option;
 
 void get_window_options();
-void set_vsync(SDL_Renderer* renderer, SDL_bool state);
+//void set_vsync(SDL_Renderer* renderer, SDL_bool state);
 void set_full_screen(SDL_Window* window, Uint32 flags);
 
 #endif
